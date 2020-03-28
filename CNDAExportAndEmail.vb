bae05809af5738f1cd8a7d89cb5349e97d3641d0
Nick O <onichola@gmail.com>
2020-03-28 09:19:35 -0700

    Refactor - moved all controller into button click

--- CNDAExportAndEmail.vb
+++ CNDAExportAndEmail.vb
@@ -13,39 +13,41 @@ Public Class CNDAExportAndEmail
             Dim m As Outlook.Inspector = e.Control.Context
             Dim mailItem As Outlook.MailItem = TryCast(m.CurrentItem, Outlook.MailItem)
             If mailItem IsNot Nothing Then
-                Dim pptFilename As String = df.GetPptFilename()
-                Dim xlsFilename As String = df.GetXlsFilename()
-                ExportAndEmailAll(pptFilename, xlsFilename, mailItem)
-                If MsgBox("Email generation complete. See your Drafts folder." & vbCrLf & "Do you with to remove the current email?", MsgBoxStyle.YesNo) = MsgBoxResult.Yes Then
-                    mailItem.Close(Outlook.OlInspectorClose.olDiscard)
+                Dim xlCndaInfo As New CndaAllInfo()
+                'TODO create constructor of CndaAllInfo with Excel filename)
+                xlCndaInfo = CndaExcel.ExtractCndaInfo(df.GetXlsFilename)
+                If CNDAPowerPoint.PptToPDFs(df.GetPptFilename, xlCndaInfo) > 0 Then
+                    For Each c As CndaInfo In xlCndaInfo.CndaInfos
+                        CreateEmailWithAttachment(CNDAPowerPoint.CndaPdfString(df.GetPptFilename, c.Cnda, c.CustName), c,
+                            mailItem)
+                    Next
+                    If MsgBox("Email generation complete. See your Drafts folder." & vbCrLf & "Do you with to remove the current email?", MsgBoxStyle.YesNo) = MsgBoxResult.Yes Then
+                        mailItem.Close(Outlook.OlInspectorClose.olDiscard)
+                    End If
                 End If
             End If
         End If
     End Sub
-    Private Sub ExportAndEmailAll(pptFilename As String, xlsFilename As String, ByVal mailItem As Outlook.MailItem)
-        Dim xlCndaInfo As New CndaAllInfo()
-        xlCndaInfo = CndaExcel.ExtractCndaInfo(xlsFilename)
-        If CNDAPowerPoint.PptToPDFs(pptFilename, xlCndaInfo) > 0 Then
-            For Each c As CndaInfo In xlCndaInfo.CndaInfos
-                Dim pdfname As String = CNDAPowerPoint.CndaPdfString(pptFilename, c.Cnda, c.CustName)
-                CreateCndaEmail(pdfname, c, mailItem)
-            Next
-        End If
-    End Sub
 
-    Private Sub CreateCndaEmail(pdfFilename As String, info As CndaBaseClasses.CndaInfo, RefMail As Outlook.MailItem)
+    ''' <summary>
+    ''' Create a copy of a reference email based on the Cnda Info, attaches a file and moves to current draft folder
+    ''' </summary>
+    ''' <param name="AttachmentName"></param>
+    ''' <param name="Info"></param>
+    ''' <param name="RefMail"></param>
+    Private Sub CreateEmailWithAttachment(AttachmentName As String, Info As CndaBaseClasses.CndaInfo, RefMail As Outlook.MailItem)
         If (RefMail IsNot Nothing) Then
             Dim curMail As Outlook.MailItem = RefMail.Copy
-            Dim unused = curMail.Attachments.Add(Source:=pdfFilename)
-            For Each c As String In info.ToList
+            Dim unused = curMail.Attachments.Add(Source:=AttachmentName)
+            For Each c As String In Info.ToList
                 Dim recipient1 As Outlook.Recipient = curMail.Recipients.Add(c)
                 recipient1.Type = Outlook.OlMailRecipientType.olTo
             Next
-            For Each c As String In info.CcList
+            For Each c As String In Info.CcList
                 Dim recipient1 As Outlook.Recipient = curMail.Recipients.Add(c)
                 recipient1.Type = Outlook.OlMailRecipientType.olCC
             Next
-            For Each c As String In info.BccList
+            For Each c As String In Info.BccList
                 Dim recipient1 As Outlook.Recipient = curMail.Recipients.Add(c)
                 recipient1.Type = Outlook.OlMailRecipientType.olBCC
             Next
