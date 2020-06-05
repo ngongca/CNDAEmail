3de276447baf2018436a138301c486512bd1ccf3
ngongca <onichola@gmail.com>
2020-06-04 21:26:12 -0700

    Bug Fix - enabled XML file persistence for CNDAPowerpoint

--- CNDAPowerPoint/CndaPptModel.vb
+++ CNDAPowerPoint/CndaPptModel.vb
@@ -1,23 +1,26 @@
 ﻿
+Imports System.IO
+
 Public Class CndaPptModel
     Public ReadOnly Property CustInfoList As New List(Of CndaCustInfo)
     Public Property XmlFilename As String
 
     Public Sub New()
-        If My.Settings.PptXmlFilename <> "" Then
+        If File.Exists(My.Settings.PptXmlFilename) Then
             XmlFilename = My.Settings.PptXmlFilename
-            If XmlFilename <> "" Then
-                CustInfoList.Clear()
-                For Each c As CndaCustInfo In CndaXmlToCustInfo(XmlFilename)
-                    CustInfoList.Add(c)
-                Next
-            End If
+            CustInfoList.Clear()
+            For Each c As CndaCustInfo In CndaXmlToCustInfo(XmlFilename)
+                CustInfoList.Add(c)
+            Next c
+        Else
+            XmlFilename = ""
         End If
     End Sub
 
     Public Sub UpdateModel(ByVal XmlFilename As String)
         Me.XmlFilename = XmlFilename
         My.Settings.PptXmlFilename = XmlFilename
+        My.Settings.Save()
         CustInfoList.Clear()
         For Each c As CndaCustInfo In CndaXmlToCustInfo(XmlFileName:=Me.XmlFilename)
             CustInfoList.Add(c)
